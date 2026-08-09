d742038d9495ccacfbb359e843c32f56a3eaf276
Pedro Henrique <pedrotxc@gmail.com>
2026-08-09 16:40:24 -0300

    first commit

---
+++ src/main/java/dev/pedrotxc/desafio_sistema_de_reserva_de_restaurante/DesafioSistemaDeReservaDeRestauranteApplication.java
@@ -0,0 +1,13 @@
+package dev.pedrotxc.desafio_sistema_de_reserva_de_restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSistemaDeReservaDeRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSistemaDeReservaDeRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pedrotxc/desafio_sistema_de_reserva_de_restaurante/DesafioSistemaDeReservaDeRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pedrotxc.desafio_sistema_de_reserva_de_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSistemaDeReservaDeRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
